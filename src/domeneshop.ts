24b4a9fc534d09d4b43e73ef9be34fe64517054f
Kent Varmedal <kent@domeneshop.no>
2020-07-27 15:15:22 +0200

    Update metadata for v0.1.8

--- src/domeneshop.ts
+++ src/domeneshop.ts
@@ -9,7 +9,7 @@ import { IDomainName } from "./lib/interfaces/domainname";
  * Main class of the Domeneshop Javascript API.
  */
 class Domeneshop {
-    public readonly version: string = "0.1.7";
+    public readonly version: string = "0.1.8";
 
     /**
      * Public API modules
